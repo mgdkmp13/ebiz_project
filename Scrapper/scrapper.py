3791a2fd8239940a4cd3b39d2af44527c2f62428
Iwo_Czartowski <Iwo@czart.net>
2024-11-15 22:55:15 +0100

    scraper now scraps such product informations: name, price, images miniature in low resolution, large images in high resolution, description, details, sizes, colours as buttons and in dropdown

--- Scrapper/scrapper.py
+++ Scrapper/scrapper.py
@@ -1,44 +1,80 @@
 import requests
 from bs4 import BeautifulSoup
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
 import json
+import os
 
 # Base URL for the Warhammer merchandise website
 baseUrl = 'https://merch-eur.warhammer.com'
 
+# Function to extract product details from the product page
+def get_info_from_product_page(product_page_url: str) -> Tuple[List[str], Optional[str], Optional[str], List[str], List[str], List[str]]:
+    page = requests.get(product_page_url)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-# Function to retrieve product data (name, price, image sources) from a category page
-def get_products_data(page_url: str) -> List[Tuple[str, str, List[str]]]:
+    big_product_images = ['https:' + image_link.get('href') for image_link in soup.find_all('a', class_='c-gallery__modal')]
+
+    product_descript_and_details = soup.find_all('div', class_='c-info-tabs__content rte')
+    product_description = product_descript_and_details[0].text.strip() if len(product_descript_and_details) > 0 else None
+    product_details = product_descript_and_details[1].text.strip() if len(product_descript_and_details) > 1 else None
+
+    product_sizes = [prod_size.text.strip() for prod_size in soup.find_all('label', class_='c-product-options__swatch -is-size')]
+    product_colours_button = [prod_colour.text.strip() for prod_colour in soup.find_all('label', class_='c-product-options__swatch -is-color')]
+
+    colours_dropdown = soup.find('select', attrs={'name': 'options[Colour]'})
+    product_colours_dropdown = [option.text.strip() for option in colours_dropdown.find_all('option')] if colours_dropdown else []
+
+    return (
+        big_product_images,
+        product_description,
+        product_details,
+        product_sizes,
+        product_colours_button,
+        product_colours_dropdown
+    )
+
+# Function to scrape product data from a category page
+def get_products_data(page_url: str) -> List[dict]:
     page = requests.get(page_url)
     soup = BeautifulSoup(page.content, 'html.parser')
 
     no_products_message = soup.find('p', class_='c-collections__message')
 
     if no_products_message:
-      return None
+        return []
 
     products = soup.find_all('article', class_='c-gallery-item')
     product_data_list = []
 
     for product in products:
-        name = product.find('h2', class_='c-product-listing__title')
-        price = product.find('span', class_='price')
+        name_element = product.find('h2', class_='c-product-listing__title')
+        price_element = product.find('span', class_='price')
         img_tags = product.find_all('img', class_='image')
-        img_src = [tag.get('data-src') for tag in img_tags if tag]
+        img_src = ['https:' + tag.get('data-src') for tag in img_tags if tag]
+        product_page = product.find('a', class_='c-gallery-item__link').get('href')
 
-        if name and price:
+        prod_info_from_page = get_info_from_product_page(baseUrl + product_page) if product_page else ([], None, None, [], [], [])
+
+        if name_element and price_element:
             product_data = {
-                "name": name.text.strip(),
-                "price": price.text.strip(),
-                "images": img_src
+                "name": name_element.text.strip(),
+                "price": price_element.text.strip(),
+                "images": img_src,
+                "detailed_images": prod_info_from_page[0],
+                "description": prod_info_from_page[1],
+                "details": prod_info_from_page[2],
+                "sizes": prod_info_from_page[3],
+                "colours_button": prod_info_from_page[4],
+                "colours_dropdown": prod_info_from_page[5]
             }
             product_data_list.append(product_data)
 
     return product_data_list
 
-def fetch_all_pages_in_category(category_name: str, max_page_iters: int):
+# Fetch all products from a category
+def fetch_all_pages_in_category(category_name: str, max_page_iters: int = 1):
     page_number = 1
     all_products = []
 
@@ -47,7 +83,7 @@ def fetch_all_pages_in_category(category_name: str, max_page_iters: int):
         print(f"Fetching data from: {page_url}")
         products = get_products_data(page_url)
 
-        if products is None:
+        if not products:
             print(f"No more products found in category '{category_name}' on page {page_number}.")
             break
         all_products.extend(products)
@@ -56,16 +92,13 @@ def fetch_all_pages_in_category(category_name: str, max_page_iters: int):
     print(f"Total products found in '{category_name}': {len(all_products)}")
     return all_products
 
-
+# Extract categories without subcategories
 def get_categories(soup):
-    # Finding categories without subcategories
     category_nav = soup.find('nav', class_='main-menu')
     category_buttons = category_nav.find_all('a', class_='main-menu__button')
-    category_no_subcategories = [button.text.strip() for button in category_buttons]
+    return [button.text.strip() for button in category_buttons]
 
-    return category_no_subcategories
-
-
+# Extract dropdown categories
 def get_dropdown_categories(soup):
     dropdown_cats_info = []
     category_with_subcategories_dropdown = soup.find_all('div', class_='main-menu__item -has-dropdown')
@@ -78,145 +111,40 @@ def get_dropdown_categories(soup):
 
     return dropdown_cats_info
 
-
-def get_mega_panel_categories(soup):
-    mega_cats_info = []
-    category_with_subcategories_mega = soup.find_all('div', class_='main-menu__item -has-mega-panel')
-
-    for cat in category_with_subcategories_mega:
-        main_cat = cat.find('button', class_='main-menu__button').text.strip()
-        subcats_info = []
-
-        for subcat in cat.find_all("div", class_="c-mega-image-panel__menu"):
-            subcat_name = subcat.find("span", class_="title").text.strip()
-            sub_sub_cats_info = [
-                (
-                    sub_sub_cat.find("h3", class_="title").text.strip(),
-                    sub_sub_cat.find("img", class_="image").get("src")
-                )
-                for sub_sub_cat in subcat.find_all("li")
-            ]
-            subcats_info.append((subcat_name, sub_sub_cats_info))
-
-        mega_cats_info.append((main_cat, subcats_info))
-
-    return mega_cats_info
-
-
-def get_regular_cats_urls(soup):
-    regular_categories = soup.find_all('a', class_='main-menu__button')
-
-    urls = []
-
-    for category in regular_categories:
-        url = category.get('href')
-        if url:
-            full_url = baseUrl + url
-            urls.append(full_url)
-
-    return urls
-
-
-def get_urls_of_subcategories(soup, main_category_name: str) -> List[str]:
-    dropdown_categories = soup.find_all('div', class_='main-menu__item -has-dropdown')
-    urls = []
-
-    for category in dropdown_categories:
-        category_button = category.find('button', class_='main-menu__button')
-
-        if category_button and category_button.text.strip() == main_category_name:
-            subcategories = category.find_all('a', class_='c-dropdown__link')
-
-            for subcat in subcategories:
-                subcat_url = subcat.get('href')
-                if subcat_url:
-                    full_url = baseUrl + subcat_url
-                    urls.append(full_url)
-            break
-
-    return urls
-
-
-def get_urls_subcategories_mega(soup):
-    urls = []
-
-    # Find major categories in the mega panel
-    mega_major_cats = soup.find_all('h2', class_='c-mega-image-panel__menu--title -js-pos-ref')
-
-    # Extract links within major categories
-    for major_cat in mega_major_cats:
-        link_tag = major_cat.find('a')
-        if link_tag:
-            url = link_tag.get('href')
-            if url:
-                full_url = baseUrl + url
-                urls.append(full_url)
-
-    # Find subcategory items within the mega panel
-    mega_categories = soup.find_all('li', class_='c-mega-image-panel__submenu--item')
-
-    # Extract links within subcategories
-    for category in mega_categories:
-        link_tag = category.find('a')
-        if link_tag:
-            url = link_tag.get('href')
-            if url:
-                full_url = baseUrl + url
-                urls.append(full_url)
-
-    return urls
-
-
-# Main scraping function to get category structure and product data
+def save_to_json(data):
+    scrapper_folder = os.path.dirname(os.path.abspath(__file__))
+    results_folder = os.path.join(scrapper_folder, 'scraping_results')
+    if not os.path.exists(results_folder):
+        os.makedirs(results_folder)
+    with open(os.path.join(results_folder, 'warhammer_products.json'), 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=4)
+    print(f"Data saved to 'scraping_results/warhammer_products.json'.")
+    print(f"Absolute path: {os.path.join(results_folder, 'warhammer_products.json')}")
+    
+# Main function
 def main():
     page = requests.get(baseUrl)
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    # Get category information
     no_subcat_cats = get_categories(soup)
     dropdown_cats_info = get_dropdown_categories(soup)
-    #mega_cats_info = get_mega_panel_categories(soup)
 
     print("Categories with no subcategories:", no_subcat_cats)
     print("\nDropdown Categories:", dropdown_cats_info)
-    #print("\nMega Panel Categories:", mega_cats_info)
-
-    non_drop_urls = get_regular_cats_urls(soup)
-    drop_urls_product = get_urls_of_subcategories(soup, "SHOP BY PRODUCT")
-    drop_urls_brand = get_urls_of_subcategories(soup, "SHOP BY BRAND")
-    #mega_urls = get_urls_subcategories_mega(soup)
-
-    print("\nRegular Categories URLs:", non_drop_urls)
-    print("\nProduct Subcategories URLs:", drop_urls_product)
-    print("\nBrand Subcategories URLs:", drop_urls_brand)
-    #print("\nMega Panel Subcategories URLs:", mega_urls)
 
     # Prepare to fetch product data for dropdown categories
     cat_url_names = [subcat.replace(' ', '-').lower() for cat in dropdown_cats_info for subcat in cat[1]]
     all_cats_prod_info = {}
 
-    fetch_with_max_iters = partial(fetch_all_pages_in_category, max_page_iters=1)
-
-    # Fetch product data for dropdown categories
+    fetch_with_max_iters = partial(fetch_all_pages_in_category, max_page_iters=6)
+    product_count = 0
     with ThreadPoolExecutor() as executor:
-      results = executor.map(fetch_with_max_iters, cat_url_names)
-
-      for cat, prod_info in zip(cat_url_names, results):
-        all_cats_prod_info[cat] = prod_info
-
-
-    # Uncomment the next line to see all products info
-    counter = 0
-    for cat in all_cats_prod_info:
-        counter += len(cat[1])
-        
-    print(counter)
-    print(no_subcat_cats)
-    print(dropdown_cats_info)
-
-    with open('warhammer_products.json', 'w', encoding='utf-8') as json_file:
-        json.dump(all_cats_prod_info, json_file, ensure_ascii=False, indent=4)
-
+        results = executor.map(fetch_with_max_iters, cat_url_names)
+        product_count += sum([len(products) for products in results])
+        for cat, prod_info in zip(cat_url_names, results):
+            all_cats_prod_info[cat] = prod_info
+    print(f"Total products fetched: {product_count}")
+    save_to_json(all_cats_prod_info)
 
 # Execute main function
 if __name__ == "__main__":

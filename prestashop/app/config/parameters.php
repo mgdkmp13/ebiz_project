e2d9abebf82d864463f9421a1757b701fbda8362
Iwo_Czartowski <122527397+IwsonHD@users.noreply.github.com>
2025-01-13 01:21:04 +0100

    Final fixes for deployment (hopefully...) (#47)

--- prestashop/app/config/parameters.php
+++ prestashop/app/config/parameters.php
@@ -1,7 +1,7 @@
 <?php return array (
   'parameters' => 
   array (
-    'database_host' => 'admin-mysql_default',
+    'database_host' => 'admin-mysql_db',
     'database_port' => '3306',
     'database_name' => 'BE_193066',
     'database_user' => 'root',
